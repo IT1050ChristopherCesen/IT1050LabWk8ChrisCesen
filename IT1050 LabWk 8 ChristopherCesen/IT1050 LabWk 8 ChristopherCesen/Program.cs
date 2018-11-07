655ba6c2c26b36f2bcffde143b54baed4ebb6cf3
IT1050ChristopherCesen <43357952+IT1050ChristopherCesen@users.noreply.github.com>
2018-11-07 14:43:29 -0800

    Add files via upload

---
+++ IT1050 LabWk 8 ChristopherCesen/IT1050 LabWk 8 ChristopherCesen/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT1050_LabWk_8_ChristopherCesen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Question 1
+ int i = 1;
+            int counter = 1;
+                while (counter <=9)
+            {
+               counter +=1;
+                Console.WriteLine(counter + i);
+                if (i > 9)
+                    break;
+				
+Question 2
+int  countNum= 45;
+            
+            for (int i = 35; i <= countNum; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(" {0} is even", i);
+                }
+                else
+                {
+                    Console.WriteLine(" {0} is odd", i);
+                }
+
+Question 3
+int militaryTime;
+
+            Console.WriteLine("Enter the time ");
+            militaryTime = Convert.ToInt32(Console.ReadLine());
+
+
+            if (militaryTime <= 11)
+            {
+                Console.Write("Good Morning");
+            }
+            else if (militaryTime <= 16)
+            {
+                Console.WriteLine("Good Afternoon");
+            }
+										
+            else
+            {
+                Console.WriteLine("Good Afternoon");
+            }	
+
+
+
+
+
+Question 4
+ int i = 9;
+            while (i <20)
+            {
+                ++i;
+                Console.WriteLine(i);
+            }
+            //Changed the value int and added an increment	
+
+Question 5      
+  /*for (int i = 0; i < 101; i++)
+                //Console.WriteLine("*******");
+                Console.WriteLine(i);
+            Console.WriteLine();
+            Console.WriteLine("*******");
+            
+            Question 6
+            for (int a = 1; a <= 10; a++)
+            {
+                for (int b = 1; b < a; b++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+*/
+            }
+        }
+    }
+    }
+}
